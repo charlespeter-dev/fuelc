e09e1365754677a30f349c82fe466c441787f6bd
AzureAD\CharlesPeter <charles.peter@2x.marketing>
2022-12-06 23:14:31 +0800

    research questions

--- _internal-linking/research-question.php
+++ _internal-linking/research-question.php
@@ -20,7 +20,20 @@ $links = [
     'https://fuelcycle.com/blog/how-market-research-takes-gaas-to-the-next-level/'    => 'research questions',
 ];
 
-
-
+$replace_counter = [
+    'https://fuelcycle.com/blog/usability-research-how-to/'    => 1,
+    'https://fuelcycle.com/blog/qualitative-interviewing/'    => 1,
+    'https://fuelcycle.com/blog/are-these-5-things-disrupting-your-market-research-status-quo/'    => 1,
+    'https://fuelcycle.com/blog/qualitative-studies/'    => 1,
+    'https://fuelcycle.com/blog/pilot-test-comparison/'    => 1,
+    'https://fuelcycle.com/blog/final-effective-steps-market-research/'    => 1,
+    'https://fuelcycle.com/blog/participant-observation-research/'    => 1,
+    'https://fuelcycle.com/blog/how-to-use-a-focus-group-to-perform-market-research/'    => 1,
+    'https://fuelcycle.com/blog/what-should-enterprises-look-for-in-their-market-research-solution/'    => 1,
+    'https://fuelcycle.com/blog/naturalistic-observation-research/'    => 1,
+    'https://fuelcycle.com/blog/data-collection-methods-in-market-research/'    => 1,
+    'https://fuelcycle.com/blog/how-to-pilot-test/'    => 1,
+    'https://fuelcycle.com/blog/how-market-research-takes-gaas-to-the-next-level/'    => 1,
+];
 
 require 'engine.php';

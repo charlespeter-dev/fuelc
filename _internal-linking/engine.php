45eb6d1ce1bc5a1cb97a14be6c09862be58c8909
AzureAD\CharlesPeter <charles.peter@2x.marketing>
2022-12-06 21:23:28 +0800

    Internal Linking added replace counter

--- _internal-linking/engine.php
+++ _internal-linking/engine.php
@@ -22,24 +22,32 @@ foreach ($links as $source => $keyword) {
         $content = $query->post->post_content;
         $needle = $keyword;
         $len = strlen($needle);
-        $pos = stripos($content, $needle);
+        $start = $count = 0;
 
-        if ($pos !== false) {
-            $oldneedle = substr($content, $pos, $len);
-            $replacement = sprintf('<a href="%s" target="_blank" rel="noreferrer noopener">%s</a>', $dest, $oldneedle);
-            $content = substr_replace($content, $replacement, $pos, $len);
+        while (($pos = stripos($content,  $needle, $start)) !== false) {
+            
+            $count++;
+            $start = $pos + 1;
 
-            if (isset($argv[1]) && $argv[1] == 'save') {
+            $rpcounter = $replace_counter[$source] ?? null;
 
-                wp_update_post([
-                    'ID' => $post_id,
-                    'post_content' => $content
-                ]);
+            if ($rpcounter && ($rpcounter == $count)) {
+                $oldneedle = substr($content, $pos, $len);
+                $replacement = sprintf('<a href="%s" target="_blank" rel="noreferrer noopener">%s</a>', $dest, $oldneedle);
+                $content = substr_replace($content, $replacement, $pos, $len);
 
-                echo "Saved: ";
+                if (isset($argv[1]) && $argv[1] == 'save') {
+
+                    wp_update_post([
+                        'ID' => $post_id,
+                        'post_content' => $content
+                    ]);
+
+                    echo "Saved: ";
+                }
+
+                echo $k + 1 . " " . $post_id . " " . $replacement . "\n";
             }
-
-            echo $k + 1 . " " . $post_id . " " . $replacement . "\n";
         }
     }
 
--- _internal-linking/sample-size.php
+++ _internal-linking/sample-size.php
@@ -7,14 +7,20 @@ $dest = 'https://fuelcycle.com/blog/how-to-determine-the-correct-sample-size-for
 $links = [
     'https://fuelcycle.com/blog/final-effective-steps-market-research/'    => 'sample size',
     'https://fuelcycle.com/blog/essential-steps-marketing-research-process/'    => 'sample size',
-    'https://fuelcycle.com/blog/price-testing/'    => 'sample size',
     'https://fuelcycle.com/blog/what-should-enterprises-look-for-in-their-market-research-solution/'    => 'sample size',
     'https://fuelcycle.com/blog/certainty-cost-trade-offs-machine-learning/'    => 'sample size',
     'https://fuelcycle.com/blog/testing-101-monadic-testing/'    => 'sample size',
     'https://fuelcycle.com/blog/when-is-an-online-community-best-for-you/'    => 'sample size',
 ];
 
-
+$replace_counter = [
+    'https://fuelcycle.com/blog/final-effective-steps-market-research/'    => 2,
+    'https://fuelcycle.com/blog/essential-steps-marketing-research-process/'    => 1,
+    'https://fuelcycle.com/blog/what-should-enterprises-look-for-in-their-market-research-solution/'    => 1,
+    'https://fuelcycle.com/blog/certainty-cost-trade-offs-machine-learning/'    => 1,
+    'https://fuelcycle.com/blog/testing-101-monadic-testing/'    => 1,
+    'https://fuelcycle.com/blog/when-is-an-online-community-best-for-you/'    => 1,
+];
 
 
 require 'engine.php';

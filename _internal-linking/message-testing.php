ad47420dff49fbde3f389f8e4591a7613e373420
AzureAD\CharlesPeter <charles.peter@2x.marketing>
2022-12-06 22:31:33 +0800

    message testing

--- _internal-linking/message-testing.php
+++ _internal-linking/message-testing.php
@@ -8,11 +8,16 @@ $links = [
     'https://fuelcycle.com/blog/market-research-product-experience/'    => 'Message testing',
     'https://fuelcycle.com/blog/marketing-mistakes/'    => 'message testing',
     'https://fuelcycle.com/blog/measuring-advertising-effectiveness-why-it-matters/'    => 'message testing',
-    'https://fuelcycle.com/blog/market-research-digital-experiences/'    => 'Message testing',
+    // 'https://fuelcycle.com/blog/market-research-digital-experiences/'    => 'Message testing',
     'https://fuelcycle.com/blog/how-to-use-a-focus-group-to-perform-market-research/'    => 'message testing',
 ];
 
-
-
+$replace_counter = [
+    'https://fuelcycle.com/blog/market-research-product-experience/'    => 1,
+    'https://fuelcycle.com/blog/marketing-mistakes/'    => 1,
+    'https://fuelcycle.com/blog/measuring-advertising-effectiveness-why-it-matters/'    => 1,
+    // 'https://fuelcycle.com/blog/market-research-digital-experiences/'    => 1,
+    'https://fuelcycle.com/blog/how-to-use-a-focus-group-to-perform-market-research/'    => 1,
+];
 
 require 'engine.php';

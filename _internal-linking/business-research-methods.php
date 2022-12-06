5f63e04034889a405a17572f4480e006f3919dca
AzureAD\CharlesPeter <charles.peter@2x.marketing>
2022-12-06 22:51:06 +0800

    research method

--- _internal-linking/business-research-methods.php
+++ _internal-linking/business-research-methods.php
@@ -22,6 +22,22 @@ $links = [
 ];
 
 
+$replace_counter = [
+    'https://fuelcycle.com/blog/boosting-engagement-through-design/'    => 1,
+    'https://fuelcycle.com/blog/the-3-most-common-observation-research-methods/'    => 1,
+    'https://fuelcycle.com/blog/the-best-research-techniques-for-your-business/'    => 1,
+    'https://fuelcycle.com/blog/controlled-observation-research/'    => 1,
+    'https://fuelcycle.com/blog/participant-observation-research/'    => 1,
+    'https://fuelcycle.com/blog/data-collection-methods-in-market-research/'    => 1,
+    'https://fuelcycle.com/blog/naturalistic-observation-research/'    => 1,
+    'https://fuelcycle.com/blog/user-research-project-template/'    => 1,
+    'https://fuelcycle.com/blog/ux-research-with-communities/'    => 1,
+    'https://fuelcycle.com/blog/blog-fuel-cycle-max-diff/'    => 1,
+    'https://fuelcycle.com/blog/in-depth-interviewing/'    => 1,
+    'https://fuelcycle.com/blog/pilot-testing-how-it-can-improve-research/'    => 1,
+    'https://fuelcycle.com/blog/research-panels/'    => 1,
+    'https://fuelcycle.com/blog/the-importance-of-sentiment-analysis-in-the-age-of-social-media/'    => 1,
+];
 
 
 require 'engine.php';

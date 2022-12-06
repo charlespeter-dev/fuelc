1050842fd366707611366ef81005f7c2b869b385
AzureAD\CharlesPeter <charles.peter@2x.marketing>
2022-12-06 21:41:28 +0800

    Internal Linking added replace counter

--- _internal-linking/ux-research.php
+++ _internal-linking/ux-research.php
@@ -20,4 +20,20 @@ $links = [
     'https://fuelcycle.com/blog/4-benefits-of-agile-marketing/'    => 'UX research',
 ];
 
+$replace_counter = [
+    'https://fuelcycle.com/blog/improve-ux-research/'    => 4,
+    'https://fuelcycle.com/blog/5-ux-research-tools-strategies-for-2022/'    => 2,
+    'https://fuelcycle.com/blog/market-vs-ux-research/'    => 2,
+    'https://fuelcycle.com/blog/5-signs-ux-research-is-more-important-than-ever/'    => 2,
+    'https://fuelcycle.com/blog/ux-research-with-communities/'    => 2,
+    'https://fuelcycle.com/blog/user-research-questions/'    => 1,
+    'https://fuelcycle.com/blog/mobile-ux-research-redefined/'    => 2,
+    'https://fuelcycle.com/blog/the-podcast-episode-for-ux-researchers/' =>    2,
+    'https://fuelcycle.com/blog/ignition-system-usability-testing/' =>    3,
+    'https://fuelcycle.com/blog/market-research-digital-experiences/' =>    1,
+    'https://fuelcycle.com/blog/product-experience-research/'    => 2,
+    'https://fuelcycle.com/blog/fc-connect-top-themes/'    => 2,
+    'https://fuelcycle.com/blog/4-benefits-of-agile-marketing/'    => 1,
+];
+
 require 'engine.php';

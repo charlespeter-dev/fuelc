d300812d3467deb4e8149330c1ab2b5e343c11e1
AzureAD\CharlesPeter <charles.peter@2x.marketing>
2023-05-15 09:56:09 +0800

    Author Bio - partial progress

--- wp-content/themes/starter-theme-1.x/author.php
+++ wp-content/themes/starter-theme-1.x/author.php
@@ -1,21 +1,41 @@
 <?php
-/**
- * The template for displaying Author Archive pages
- *
- * Methods for TimberHelper can be found in the /lib sub-directory
- *
- * @package  WordPress
- * @subpackage  Timber
- * @since    Timber 0.1
- */
 
-global $wp_query;
+global $post, $paged, $timber;
 
-$context          = Timber::context();
-$context['posts'] = new Timber\PostQuery();
-if ( isset( $wp_query->query_vars['author'] ) ) {
-	$author            = new Timber\User( $wp_query->query_vars['author'] );
-	$context['author'] = $author;
-	$context['title']  = 'Author Archives: ' . $author->name();
+if (!isset($paged) || !$paged) {
+	$paged = 1;
 }
-Timber::render( array( 'author.twig', 'archive.twig' ), $context );
+
+$author_id = $post->post_author;
+
+$author_posts_args = [
+	'fields' => 'ids',
+	'post_status' => 'publish',
+	'author' => $author_id,
+	'orderby' => 'ID',
+	'order' => 'DESC',
+	'posts_per_page' => 10,
+	'paged' => $paged
+];
+
+$context = $timber::context();
+$context['ttcposts'] = new Timber\PostQuery($author_posts_args);
+$context['pagination'] = $timber::get_pagination();
+
+$author = [
+	'profile_picture_url' => get_field('profile_picture', 'user_' . $author_id)['url'],
+	'display_name' => get_the_author_meta('display_name', $author_id),
+	'description' => get_the_author_meta('description', $author_id),
+	'url' => get_author_posts_url($author_id)
+];
+
+$css_absolute_path = sprintf('%s/assets/css/%s.css', get_stylesheet_directory(), 'resource-hub');
+$css_url = sprintf('%s/assets/css/%s.css', get_stylesheet_directory_uri(), 'resource-hub');
+
+if (file_exists($css_absolute_path)) {
+	wp_enqueue_style('author', $css_url, [], false, 'screen');
+}
+
+
+$context['author'] = $author;
+$timber::render('author.twig', $context);
---
+++ wp-content/themes/starter-theme-1.x/page-author.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Template Name: Authors
+ */
+
+$excerpt_limit = 30;
+
+$author_ids = [
+    'fcadmin' => 8, // Victoria Shakespeare
+    'kstockman' => 4, // Kalyn Stockman
+];
+
+foreach ($author_ids as $author_id) {
+
+    $authors_posts = new WP_Query([
+        'fields' => 'ids',
+        'post_status' => 'publish',
+        'author' => $author_id,
+        'orderby' => 'ID',
+        'order' => 'DESC'
+    ]);
+
+    $profile_picture_url = get_field('profile_picture', 'user_' . $author_id)['url'];
+    $display_name = get_the_author_meta('display_name', $author_id);
+    $description = get_the_author_meta('description', $author_id);
+    $url = get_author_posts_url($author_id);
+    $post_ids = $authors_posts->posts;
+
+    /**
+     * excerpts
+     */
+
+    $description = explode(' ', $description);
+    $description = array_splice($description, 0, 30);
+    $description = implode(' ', $description);
+    $description = $description . '...';
+
+    $authors[] = [
+        'profile_picture_url' => $profile_picture_url,
+        'display_name' => $display_name,
+        'description' => $description,
+        'url' => $url,
+        'post_ids' => $post_ids
+    ];
+}
+
+// print_r($authors);
+// exit;
+
+
+$css_absolute_path = sprintf('%s/assets/css/%s.css', get_stylesheet_directory(), 'authors');
+$css_url = sprintf('%s/assets/css/%s.css', get_stylesheet_directory_uri(), 'authors');
+
+if (file_exists($css_absolute_path)) {
+    wp_enqueue_style('authors', $css_url, [], false, 'screen');
+}
+
+global $timber;
+
+$context = $timber::context();
+$context['authors'] = $authors;
+$timber::render('authors.twig', $context);

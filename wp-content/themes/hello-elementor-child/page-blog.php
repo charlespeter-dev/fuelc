3711ca13b9f4e134d8eebb5a0875eb06d922f1c5
AzureAD\CharlesPeter <charles.peter@2x.marketing>
2023-08-23 19:55:41 +0800

    update: Blog index page

--- wp-content/themes/hello-elementor-child/page-blog.php
+++ wp-content/themes/hello-elementor-child/page-blog.php
@@ -1,62 +1,19 @@
 <?php
-global $post;
 
-$fields = get_fields($post->ID);
-extract($fields);
+global $timber;
 
-get_header() ?>
+$context = $timber::context();
+$timber_post = new Timber\Post();
+$context['post'] = $timber_post;
+$args['search_filter_id'] = 27217;
+$context['ttcposts'] = new Timber\PostQuery($args);
+$context['pagination'] = $timber::get_pagination();
 
-<div class="bootstrap-container">
-    <div class="timber-oldfc">
-        <div class="container-1140">
-            <section id="resource-hub" class="margin-top-for-nav">
-                <div class="container pt-4 container-1088">
-                    <div class="horizontal-line-clean mb-5"></div>
-                    <div class="row">
-                        <div class="col-12 col-md-7">
-                            <span class="hub-featured-subtitle">
-                                <?= $subtitle ?>
-                            </span>
-                            <h1 class="section-title">
-                                <?= $section_title ?>
-                            </h1>
-                        </div>
-                        <div class="col-12 col-md-5 d-flex align-items-center">
-                            <div>
-                                <?= $intro_text ?>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="horizontal-line-clean my-5"></div>
-                </div>
-                <div class="container container-1088">
-                    <div class="row">
-                        <span class="hub-featured-section-title">
-                            <?= $featured_section_title ?>
-                        </span>
-                        <div class="col-12 col-md-7">
-                            <img class="img-fluid hub-featured-post-image"
-                                src="<?= wp_get_attachment_image_url($featured_post_image, 'full') ?>" alt="">
-                        </div>
-                        <div class="col-12 col-md-5">
-                            <h2 class="hub-featured-post-title">
-                                <?= $featured_post_title ?>
-                            </h2>
-                            <p class="hub-featured-post-paragraph">
-                                <?= $featured_post_paragraph ?>
-                            </p>
-                            <a class="hub-featured-post-link" href="<?= $featured_post_link_url ?>">
-                                <?= $featured_post_link_text ?> <i class="fa-solid fa-arrow-right"></i>
-                            </a>
-                        </div>
-                    </div>
-                    <section id="pagination-anchor"></section>
-                    <div class="horizontal-line-clean my-5"></div>
-                </div>
+get_header();
 
-            </section>
-        </div>
-    </div>
-</div>
+$timber::render(['page-blog.twig'], $context);
 
-<?php get_footer() ?>
+get_footer();
+
+
+?>

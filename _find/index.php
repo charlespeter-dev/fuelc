efc6e4a920f80fa87ef986a50e88968b70f00b2f
AzureAD\CharlesPeter <charles.peter@2x.marketing>
2024-02-28 12:12:22 +0800

    Custom Script: Find

--- _find/index.php
+++ _find/index.php
@@ -50,7 +50,7 @@ if (isset($_GET['s']) && $_GET['s']) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 
-    <title>Search &amp; Replace</title>
+    <title>Search Keyword</title>
 </head>
 
 <body>
@@ -59,7 +59,7 @@ if (isset($_GET['s']) && $_GET['s']) {
         <section>
             <div class="container">
                 <h1>
-                    Search & Replace
+                    Search Keyword
                 </h1>
             </div>
         </section>

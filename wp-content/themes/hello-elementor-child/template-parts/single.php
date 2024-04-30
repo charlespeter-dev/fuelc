225205640a491a9431087748096cfd91dc4a7dac
AzureAD\CharlesPeter <charles.peter@2x.marketing>
2024-04-30 11:47:56 +0800

    Do not display author section if the post belong to 'Platform Updates' category

--- wp-content/themes/hello-elementor-child/template-parts/single.php
+++ wp-content/themes/hello-elementor-child/template-parts/single.php
@@ -5,13 +5,13 @@
  * @package HelloElementor
  */
 
-if(!defined('ABSPATH')) {
+if (!defined('ABSPATH')) {
 	exit; // Exit if accessed directly.
 }
 
-if(!is_single()):
+if (!is_single()):
 
-	while(have_posts()):
+	while (have_posts()):
 		the_post();
 		?>
 
@@ -20,7 +20,7 @@ if(!is_single()):
 			<div class="page-content">
 				<?php the_content(); ?>
 				<div class="post-tags">
-					<?php the_tags('<span class="tag-links">'.esc_html__('Tagged ', 'hello-elementor'), null, '</span>'); ?>
+					<?php the_tags('<span class="tag-links">' . esc_html__('Tagged ', 'hello-elementor'), null, '</span>'); ?>
 				</div>
 				<?php wp_link_pages(); ?>
 			</div>
@@ -39,6 +39,22 @@ else:
 	$post = new Timber\Post();
 	$ttcposts = new Timber\PostQuery(['search_filter_id' => 27217]);
 
+	//
+	// check if post under 'Platform Updates'
+	// yes? do not display the author
+	//
+
+	$show_author = true;
+
+	if ($post->categories) {
+		foreach ($post->categories as $category) {
+			if ($category->slug == 'platform-updates') {
+				$show_author = false;
+				break;
+			}
+		}
+	}
+
 	/**
 	 * author
 	 */
@@ -46,8 +62,8 @@ else:
 	$author_id = $post->post_author;
 	$author = new Timber\User($author_id);
 
-	$profile_picture = get_field('profile_picture', 'user_'.$author_id);
-	if(!empty($profile_picture)) {
+	$profile_picture = get_field('profile_picture', 'user_' . $author_id);
+	if (!empty($profile_picture)) {
 		$profile_picture_url = $profile_picture['url'];
 		$author->profile_picture_url = $profile_picture_url;
 	} else {
@@ -56,11 +72,11 @@ else:
 	}
 
 	$description = get_the_author_meta('description', $author_id);
-	if(!empty($description)) {
+	if (!empty($description)) {
 		$description_words = explode(' ', $description);
 		$excerpt_words = array_splice($description_words, 0, 30);
 		$excerpt = implode(' ', $excerpt_words);
-		$author->description_excerpt = $excerpt.'...';
+		$author->description_excerpt = $excerpt . '...';
 	} else {
 		$author->description_excerpt = '';
 	}
@@ -83,27 +99,27 @@ else:
 
 						<article class="post-type-<?= $post->post_type ?>" id="post-<?= $post->ID ?>">
 
-							<?php if($post->categories): ?>
+							<?php if ($post->categories): ?>
 								<span class="blog-tag">
-									<?php foreach($post->categories as $category): ?>
+									<?php foreach ($post->categories as $category): ?>
 										<?= $category->name ?>
 									<?php endforeach ?>
 								</span>
 							<?php endif ?>
 
-							<?php if($post->title): ?>
+							<?php if ($post->title): ?>
 								<h1 class="article-h1">
 									<?= $post->title ?>
 								</h1>
 							<?php endif ?>
 
-							<?php if($post->thumbnail): ?>
+							<?php if ($post->thumbnail): ?>
 								<div class="my-3 py-1"></div>
 								<img src="<?= $post->thumbnail->src ?>" alt="" class="img-fluid post-featured-img">
 								<div class="my-3 py-1"></div>
 							<?php endif ?>
 
-							<?php if($post->content): ?>
+							<?php if ($post->content): ?>
 								<section class="article-content">
 									<div class="article-body">
 										<?= $post->content ?>
@@ -113,63 +129,67 @@ else:
 
 						</article>
 
-						<section class="py-3">
-							<div class="horizontal-line"></div>
-							<div class="row autho-info-box py-4">
-								<div class="col-sm-12 col-lg-4">
-									<div class="author-img-container mb-4">
-										<img class="author-img" src="<?= $author->profile_picture_url ?>"
-											alt="Author Image">
-									</div>
-								</div>
-								<div class="col-sm-12 col-lg-8">
+						<?php if ($show_author): ?>
 
-									<?php if($author->name): ?>
-										<h2 class="author-title">Author:
-											<?= $author->name ?>
-										</h2>
-									<?php endif ?>
-
-									<div class="author-socials py-2 mb-2">
-										<?php if($author->facebook): ?>
-											<a href="<?= $author->facebook ?>">
-												<i class="fa-brands fa-facebook"></i>
-											</a>
-										<?php endif ?>
-
-										<?php if($author->instagram): ?>
-											<a href="<?= $author->instagram ?>">
-												<i class="fa-brands fa-instagram"></i>
-											</a>
-										<?php endif ?>
-
-										<?php if($author->instagram): ?>
-											<a href="<?= $author->linkedin ?>">
-												<i class="fa-brands fa-linkedin"></i>
-											</a>
-										<?php endif ?>
-									</div>
-
-									<?php if($author->description_excerpt): ?>
-										<div class="author-desc">
-											<p>
-												<?= $author->description_excerpt ?>
-											</p>
-											<div>
-												<a class="main-btn brand-btn" href="<?= $author->url ?>">
-													<?= __('View Bio') ?>
-												</a>
-											</div>
+							<section class="py-3">
+								<div class="horizontal-line"></div>
+								<div class="row autho-info-box py-4">
+									<div class="col-sm-12 col-lg-4">
+										<div class="author-img-container mb-4">
+											<img class="author-img" src="<?= $author->profile_picture_url ?>"
+												alt="Author Image">
 										</div>
-									<?php else: ?>
-										<p>
-											<?= __('No description available.') ?>
-										</p>
-									<?php endif ?>
+									</div>
+									<div class="col-sm-12 col-lg-8">
 
+										<?php if ($author->name): ?>
+											<h2 class="author-title">Author:
+												<?= $author->name ?>
+											</h2>
+										<?php endif ?>
+
+										<div class="author-socials py-2 mb-2">
+											<?php if ($author->facebook): ?>
+												<a href="<?= $author->facebook ?>">
+													<i class="fa-brands fa-facebook"></i>
+												</a>
+											<?php endif ?>
+
+											<?php if ($author->instagram): ?>
+												<a href="<?= $author->instagram ?>">
+													<i class="fa-brands fa-instagram"></i>
+												</a>
+											<?php endif ?>
+
+											<?php if ($author->instagram): ?>
+												<a href="<?= $author->linkedin ?>">
+													<i class="fa-brands fa-linkedin"></i>
+												</a>
+											<?php endif ?>
+										</div>
+
+										<?php if ($author->description_excerpt): ?>
+											<div class="author-desc">
+												<p>
+													<?= $author->description_excerpt ?>
+												</p>
+												<div>
+													<a class="main-btn brand-btn" href="<?= $author->url ?>">
+														<?= __('View Bio') ?>
+													</a>
+												</div>
+											</div>
+										<?php else: ?>
+											<p>
+												<?= __('No description available.') ?>
+											</p>
+										<?php endif ?>
+
+									</div>
 								</div>
-							</div>
-						</section>
+							</section>
+
+						<?php endif ?>
 
 						<section>
 							<div class="py-3"></div>
@@ -177,7 +197,7 @@ else:
 
 							<div class="single-post-pagination">
 								<div class="newest mb-3 mb-sm-0">
-									<?php if($post->prev): ?>
+									<?php if ($post->prev): ?>
 										<div class="newest-title">
 											<?= __('OLDER') ?>
 										</div>
@@ -188,7 +208,7 @@ else:
 								</div>
 								<div class="divider-line"></div>
 								<div class="older mb-3 mb-sm-0">
-									<?php if($post->next): ?>
+									<?php if ($post->next): ?>
 										<div class="newest-title">
 											<?= __('NEWER') ?>
 										</div>
@@ -217,7 +237,7 @@ else:
 									</h2>
 
 									<div class="recommended-posts-row">
-										<?php foreach($ttcposts as $ttcpost): ?>
+										<?php foreach ($ttcposts as $ttcpost): ?>
 
 											<div class="single-post-wrapper">
 
@@ -230,22 +250,22 @@ else:
 												<div class="post-text-wrapper">
 
 													<span class="post-categories">
-														<?php foreach($ttcpost->categories as $category): ?>
+														<?php foreach ($ttcpost->categories as $category): ?>
 															<?= $category ?>
 														<?php endforeach ?>
 													</span>
 
 													<span class="single-post-title">
 														<a href="<?= $ttcpost->link ?>">
-															<?= count(explode(' ', $ttcpost->title)) > 9 ? implode(' ', array_slice(explode(' ', $ttcpost->title), 0, 9))."..." : $ttcpost->title ?>
+															<?= count(explode(' ', $ttcpost->title)) > 9 ? implode(' ', array_slice(explode(' ', $ttcpost->title), 0, 9)) . "..." : $ttcpost->title ?>
 														</a>
 													</span>
 
 													<p class="single-post-excerpt">
-														<?php if($ttcpost->post_excerpt_custom): ?>
+														<?php if ($ttcpost->post_excerpt_custom): ?>
 															<?= $ttcpost->post_excerpt_custom ?>
 														<?php else: ?>
-															<?= implode(' ', array_slice(explode(' ', $ttcpost->preview), 0, 9))."..." ?>
+															<?= implode(' ', array_slice(explode(' ', $ttcpost->preview), 0, 9)) . "..." ?>
 														<?php endif ?>
 														<a href="<?= $ttcpost->link ?>" class="read-more">
 															<i class="fa-solid fa-arrow-right"></i>
@@ -271,7 +291,7 @@ else:
 
 <?php endif ?>
 
-<?php if(isset($_GET['elementor-preview'])): ?>
+<?php if (isset($_GET['elementor-preview'])): ?>
 
 
 	<?php
@@ -280,7 +300,7 @@ else:
 	 * fix for ajax call Elementor Preview with 'the_content' error
 	 */
 
-	while(have_posts()):
+	while (have_posts()):
 		the_post();
 		the_content();
 	endwhile;

d043e97e1ee152eb8d6f8330bc1d2b8b8df75ea3
AzureAD\CharlesPeter <charles.peter@2x.marketing>
2024-06-06 11:56:52 +0800

    Changed navbar color

--- wp-content/themes/hello-elementor-child/assets/js/custom-header-menu.js
+++ wp-content/themes/hello-elementor-child/assets/js/custom-header-menu.js
@@ -317,3 +317,22 @@ document.addEventListener('DOMContentLoaded', function () {
 		document.documentElement.scrollTop = 0 // For Chrome, Firefox, IE and Opera
 	});
 });
+
+// bsync
+
+(function () {
+    try {
+        var script = document.createElement('script');
+        if ('async') {
+            script.async = true;
+        }
+        script.src = 'https://localhost:5555/browser-sync/browser-sync-client.js?v=3.0.2';
+        if (document.body) {
+            document.body.appendChild(script);
+        } else if (document.head) {
+            document.head.appendChild(script);
+        }
+    } catch (e) {
+        console.error("Browsersync: could not append script tag", e);
+    }
+})()
